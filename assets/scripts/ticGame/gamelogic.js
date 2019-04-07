1fbc4fe2a50ae23f938f521ab2503191ac4b9300
aberrayo <aberrayo@gmail.com>
2019-04-07 15:10:04 -0400

    Made updates to style, and worked on scripts.js

--- assets/scripts/ticGame/gamelogic.js
+++ assets/scripts/ticGame/gamelogic.js
@@ -1,63 +1,63 @@
 'use strict'
-
 const arr = ['', '', '', '', '', '', '', '', '']
-let move = 'x'
+let firstMove = 'x'
+
 const onClick = function (event) {
   event.preventDefault()
-  const cell = event.target
 
-  if (move==='X') {
-    move = 'O'
-
-  /*  $(cell).text(move)
-    arr[cell] = move
-    checkWin(arr, move)
-    move = 'o'*/
-  }else  {
-    move = 'X'
-  }
-
-  const content = $(cell).text(move)
-  console.log(move)
-  if (content === '' && move === 'o') {
-    (event.target.id).text(move)
-    arr[cell] = move
-    checkWin(arr, move)
-    move = 'x'
-  } else {
-    //alert('Cand Do That!')
-  }
+firstMove = firstMove === 'X' ? 'O' : 'X'
+  $(event.target).text(firstMove)
 }
+console.log(arr)
 
-let gameOver = false
-const checkWin = function (arr, move) {
+
+  /*console.log(arr)
+  if (content === '' && firstMove === 'o') {
+    $(event.target).text(firstMove)
+    console.log(event.target)*/
+    //$(cell).text(firstMove)
+    //arr[event.target] = firstMove
+  //  firstMove = 'x'
+  /*} else {
+    // alert('Cand Do That!')
+  }
+}*/
+
+ let gameOver = false
+const checkWin = function (arr) {
   console.log(checkWin)
-  console.log(move)
-  if
-  ((arr[0] === arr[1] && arr[1] === arr[2] && arr[0] === move) ||
-(arr[3] === arr[4] && arr[4] === arr[5] && arr[3] === move) ||
-(arr[6] === arr[7] && arr[7] === arr[8] && arr[6] === move) ||
-(arr[0] === arr[4] && arr[4] === arr[8] && arr[0] === move) ||
-(arr[2] === arr[4] && arr[4] === arr[6] && arr[2] === move) ||
-(arr[0] === arr[3] && arr[3] === arr[6] && arr[0] === move) ||
-(arr[1] === arr[4] && arr[4] === arr[7] && arr[1] === move) ||
-(arr[2] === arr[5] && arr[5] === arr[8] && arr[2] === move)) {
+  console.log(firstMove)
+  if ((arr[0] === arr[1] && arr[1] === arr[2] && arr[0] === firstMove) ||
+(arr[3] === arr[4] && arr[4] === arr[5] && arr[3] === firstMove) ||
+(arr[6] === arr[7] && arr[7] === arr[8] && arr[6] === firstMove) ||
+(arr[0] === arr[4] && arr[4] === arr[8] && arr[0] === firstMove) ||
+(arr[2] === arr[4] && arr[4] === arr[6] && arr[2] === firstMove) ||
+(arr[0] === arr[3] && arr[3] === arr[6] && arr[0] === firstMove) ||
+(arr[1] === arr[4] && arr[4] === arr[7] && arr[1] === firstMove) ||
+(arr[2] === arr[5] && arr[5] === arr[8] && arr[2] === firstMove)) {
     gameOver = true
-    alert(move + ' is the winner!')
-  } else if (arr.every(index => index !== '')) {
+    alert(firstMove + ' is the winner!')
+  } else if (array.length === 9) {
+    console.log('Draw')
     //gameOver = true
     console.log('its a tie! ')
   }
 }
+function button () {
+  location.reload()
+}
 const addHandlers = function () {
   $('.unit').on('click', onClick)
-  console.log(addHandlers)
+  $('#button').on('click', button)
+  $('#account-wrapper').hide()
+}
+  console.log(checkWin)
   /*$('#new-game').on('click', onNewGame)
   $('#new-user').on('click', newUserclick)
   $('#returning-user').on('click', returningUserclick)
   $('#user-management').on('click', accountClick)
   $('#reset-game').on('click', resetClick)8*/
-}
+
 module.exports = {
   addHandlers
 }
