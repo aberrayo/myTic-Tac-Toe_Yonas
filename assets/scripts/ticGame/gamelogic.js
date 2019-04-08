ca22b001dabb8f9dca24b0a9da964d9868128547
aberrayo <aberrayo@gmail.com>
2019-04-08 08:30:15 -0400

    Modified script files and html Files

--- assets/scripts/ticGame/gamelogic.js
+++ assets/scripts/ticGame/gamelogic.js
@@ -1,27 +1,35 @@
 'use strict'
 const arr = ['', '', '', '', '', '', '', '', '']
+
 let firstMove = 'x'
 
 const onClick = function (event) {
   event.preventDefault()
 
 firstMove = firstMove === 'X' ? 'O' : 'X'
-  $(event.target).text(firstMove)
-}
+  $(event.target.unit1).text(firstMove)
+  for (let i = 0; i < $('.unit').length; i++) {
+    arr[i] = firstMove
 console.log(arr)
+console.log(arr.length)
+  }
 
+  // const $content = $(event.target)
 
-  /*console.log(arr)
-  if (content === '' && firstMove === 'o') {
+// function placeTaken () {
+
+  /* if ($content.text() === '' && firstMove === 'o') {
     $(event.target).text(firstMove)
-    console.log(event.target)*/
-    //$(cell).text(firstMove)
-    //arr[event.target] = firstMove
-  //  firstMove = 'x'
-  /*} else {
+    console.log(event.target) */
+//  }
+}
+// $(cell).text(firstMove)
+// arr[event.target] = firstMove
+//  firstMove = 'x'
+/* } else {
     // alert('Cand Do That!')
   }
-}*/
+} */
 
  let gameOver = false
 const checkWin = function (arr) {
